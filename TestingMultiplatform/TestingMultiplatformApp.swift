fd005f965aa48214935ff6581b53db0e496d24a3
Roberto Rojo <rrojo.va@gmail.com>
2022-11-01 04:48:02 +0100

    Initial Commit

---
+++ TestingMultiplatform/TestingMultiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingMultiplatformApp.swift
+//  TestingMultiplatform
+//
+//  Created by Roberto Rojo Sahuquillo on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TestingMultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
